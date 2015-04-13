932dbeefba864936f2b6cb069d61802fe875ccac
shelbsimone <shelby.williams22@gmail.com>
2015-04-13 13:29:27 +0000

    adding dropbox

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141028173003) do
+ActiveRecord::Schema.define(version: 20150408171758) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141028173003) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
