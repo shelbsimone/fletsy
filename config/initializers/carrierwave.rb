12d0946cdd0cd484cb95b61fd156bc89e61ed379
shelbsimone <shelby.williams22@gmail.com>
2015-04-18 19:42:34 +0000

    added figaro

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.dropbox_app_key = "lk1319b984cmh3t"
-  config.dropbox_app_secret = "vv8bndx66u7b2u4"
+  config.dropbox_app_secret = ENV["APP_SECRET"]
   config.dropbox_access_token = "i2gjsy4o2hhox4q2"
-  config.dropbox_access_token_secret = "ggct045459qpnme"
+  config.dropbox_access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
   config.dropbox_user_id = "255415007"
 	config.dropbox_access_type = "app_folder"
 end
