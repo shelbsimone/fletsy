fdb84eb0a16c406bdf5263a98ed594ee12abc47a
shelbsimone <shelby.williams22@gmail.com>
2015-04-22 10:54:10 +0000

    set user permissions n stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-	
-	class ApplicationController < ActionController::Base
-		before_filter :configure_permitted_parameters, if: :devise_controller?
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
 
-  protected
+	protect_from_forgery with: :exception
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
-		devise_parameter_sanitizer.for(:account_update) << :username
-  end
-end
+	before_filter :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.for(:sign_up) << :name
+		devise_parameter_sanitizer.for(:account_update) << :name
+	end
 end
