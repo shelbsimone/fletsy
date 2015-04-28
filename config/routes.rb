38912375fca782a90e285442458fe72ea97f9380
shelbsimone <shelby.williams22@gmail.com>
2015-04-28 00:55:18 +0000

    fixed purchase issues

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   get 'pages/contact'
 	
 	get 'seller' => "listings#seller"
+	get 'sales' => "orders#sales"
+	get 'purchases' => "orders#purchases"
   
   root 'listings#index'
 
